0d100c845012eec2ecddb1409052f54976eec7c3
evermake <vladeryabkin@gmail.com>
2025-12-07 12:06:30 +0500

    add knip

---
+++ knip.config.ts
@@ -0,0 +1,34 @@
+import type { KnipConfig } from 'knip'
+
+const config: KnipConfig = {
+  entry: ['src/index.ts!'],
+  project: ['src/**/*.ts!'],
+  rules: {
+    files: 'error',
+    dependencies: 'error',
+    devDependencies: 'error',
+    optionalPeerDependencies: 'error',
+    unlisted: 'error',
+    binaries: 'error',
+    catalog: 'error',
+    unresolved: 'error',
+    exports: 'error',
+    nsExports: 'error',
+    classMembers: 'error',
+    enumMembers: 'error',
+    types: 'error',
+    nsTypes: 'error',
+    duplicates: 'error',
+  },
+  eslint: {
+    config: 'eslint.config.js',
+  },
+  typescript: {
+    config: [
+      'tsconfig.dev.json',
+      'tsconfig.lib.json',
+    ],
+  },
+}
+
+export default config
