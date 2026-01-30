ab5be018816328d13b75e3cdecbe0e34711e711c
evermake <vladeryabkin@gmail.com>
2026-01-30 22:53:12 +0500

    add testcase

---
+++ test/BotApi.test-d.ts
@@ -0,0 +1,16 @@
+import { describe, it } from 'vitest'
+import * as BotApi from '../src/BotApi.ts'
+
+describe('BotApi', () => {
+  describe('callMethod', () => {
+    it('should not typecheck if no params passed for methods with required params', () => {
+      // @ts-expect-error name is a string
+      BotApi.callMethod('sendMessage')
+    })
+    it('should typecheck if no params passed for methods with all params optional', () => {
+      BotApi.callMethod('getMe')
+      BotApi.callMethod('getAvailableGifts')
+      BotApi.callMethod('getUpdates')
+    })
+  })
+})
